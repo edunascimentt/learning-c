3e47a9e80ca8bc11b0f70fce181001da41449b92
edunascimentt <joaoeduardoeduardodev2212@gmail.com>
2026-03-12 00:37:25 -0300

    Add first C program to demonstrate variable declaration and output

---
+++ firstcode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    //declarando as variáveis
+    int meuinteiro = 10;
+    double meudouble = 1800.7;
+    char meuchar = 'J';
+    float meufloat = 15.20;
+
+    printf("\n", meuinteiro);
+
+    return 0;
+}
