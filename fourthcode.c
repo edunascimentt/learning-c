04f59447dd288e8f91e0fd9d8740516973459d71
edunascimentt <joaoeduardodev2212@gmail.com>
2026-03-19 21:42:30 -0300

    Add fifth and fourth C programs demonstrating conditional statements and binary outputs

--- fourthcode.c
+++ fourthcode.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    
+    int numero = 15;
+
+    if (numero == 10)
+    {
+        printf("O número é igual a 10 \n");
+    }
+
+    else if (numero >= 10)
+    {
+        printf("O número é maior que 10 \n");
+    }
+
+    else
+    {
+        printf("O número é menor que 10");
+    }
+
+    return 0;
 }
